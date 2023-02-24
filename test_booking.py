e60515be82530fd11125e5ca9d588b440096cade
Martha Osemwinyen <idoghormat@gmail.com>
2023-02-24 11:21:25 +0200

    new updated

--- test_booking.py
+++ test_booking.py
@@ -3,5 +3,5 @@ import requests
 json4 = {"bookingdates":{"checkin":"","checkout":""},"depositpaid":False,"firstname":"","lastname":"","roomid":1,"email":"","phone":""}
 
 def test_booking_empty_fields():
-    response = requests.post("http://localhost/booking/", json = json4)
+    response = requests.post("http://localhost:3000/booking/", json = json4)
     assert response.status_code == 400
--- test_message.py
+++ test_message.py
@@ -14,3 +14,14 @@ def test_message_api_creats_new_messages():
     })
     assert response.status_code == 201
     print(response.text)
+
+def test_message_api_creats_new_messages():
+    response = requests.post(url, json={
+        'description':'Hello, is room 102 vacant?',
+        'email':'smith@email.com',
+        'name':'smith',
+        'phone':'04506797000',
+        'subject':'room 102 vacant?',
+    })
+    assert response.status_code == 201
+    print(response.text)
